f306f5b9d40098e5d12745ccd82cac29b00be51f
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2022-01-21 04:37:12 -0500

    completed task class

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,19 @@
 function Main({ todoList }) {
+    const toggleChecked = (e) => {
+        const task = e.target;
+        if (task.className === "task-complete") {
+            return task.className = ''
+        }
+        task.className = 'task-complete';
+    }
+
     return (
         <div className="main-container">
             <div className='task-list-container'>
                 <ul className="task-list">
                     {todoList.map( (task, i) => {
                         return (
-                            <li key={i}>{task}</li>
+                            <li onClick={toggleChecked} key={i}>{task}</li>
                         )
                     })}
                 </ul>
