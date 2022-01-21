063317c00e1843d49b55ce4050c067f94e14ed46
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2022-01-21 00:25:46 -0500

    modal and header state handlers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import { useState, useEffect } from 'react';
+import { Modal, Button, Input } from 'antd';
+
 function App() {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isButtonVisible, setIsButtonVisible] = useState(false);
+    const [username, setUsername] = useState('');
+    const [changeUsername, setChangeUsername] = useState('');
+
+    const handleIsBtnVis = (e) => {
+        setIsButtonVisible(!isButtonVisible);
+    }
+
+    const handleOnChange = (e) => {
+        const name = e.target.value;
+        setChangeUsername(name);
+    }
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+    
+    const handleOk = () => {
+        setUsername(changeUsername);
+        setIsButtonVisible(false);
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsButtonVisible(false);
+        setIsModalVisible(false);
+    };
+
+    useEffect(() => {
+        window.addEventListener("load", (e) => {
+            showModal();
+        })
+    },[])
+
     return (
-        <div>
-            This is the App!
+        <div className='App'>
+            <div className='head-container'>
+                <Button type="primary" onClick={showModal} disabled={!isButtonVisible?true:false}>
+                    User: {!username ? "Anonomous" : username}
+                </Button>
+                <h1 onClick={handleIsBtnVis}><span className='hov-point'>{!username ? '' : username+"'s"}</span> To-Do List!</h1>
+            </div>
+        <Modal title="Enter your name..." visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <Input placeholder="User?" onChange={handleOnChange} onPressEnter={handleOk} value={changeUsername} />
+        </Modal>
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App'
+import App from './App';
+import './styles/index.css';
+import './styles/App.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+    <App/>,
     document.getElementById('root')
 );
